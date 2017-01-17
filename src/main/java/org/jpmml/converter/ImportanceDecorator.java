1804ab220762239059f19cdb6815fd4e39dfeec0
Villu Ruusmann <villu.ruusmann@gmail.com>
2017-01-17 13:58:30 +0200

    Added class ImportanceDecorator

---
+++ src/main/java/org/jpmml/converter/ImportanceDecorator.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2017 Villu Ruusmann
+ *
+ * This file is part of JPMML-Converter
+ *
+ * JPMML-Converter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JPMML-Converter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with JPMML-Converter.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jpmml.converter;
+
+import org.dmg.pmml.DataField;
+import org.dmg.pmml.MiningField;
+
+public class ImportanceDecorator implements FieldDecorator {
+
+	private Double importance = null;
+
+
+	@Override
+	public void decorate(DataField dataField, MiningField miningField){
+		miningField.setImportance(getImportance());
+	}
+
+	public Double getImportance(){
+		return this.importance;
+	}
+
+	public ImportanceDecorator setImportance(Double importance){
+		this.importance = importance;
+
+		return this;
+	}
+}
