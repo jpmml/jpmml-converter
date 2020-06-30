8ebe94ba9e852ae3e57898857b8505b2538c4554
Villu Ruusmann <villu.ruusmann@gmail.com>
2020-06-30 21:35:10 +0300

    Validated the missing value replacement value. Fixes jpmml/jpmml-sklearn#139

--- src/main/java/org/jpmml/converter/MissingValueDecorator.java
+++ src/main/java/org/jpmml/converter/MissingValueDecorator.java
@@ -35,6 +35,12 @@ public class MissingValueDecorator implements Decorator {
 			if(missingValueReplacement != null){
 				throw new IllegalArgumentException();
 			}
+		} else
+
+		{
+			if(missingValueReplacement != null && ValueUtil.isNaN(missingValueReplacement)){
+				throw new IllegalArgumentException("Expected a valid value as a missing value replacement value, got an invalid value (NaN)");
+			}
 		}
 
 		setMissingValueTreatment(missingValueTreatment);
