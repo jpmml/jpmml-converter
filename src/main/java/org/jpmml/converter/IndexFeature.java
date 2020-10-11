297989837f2e0abc49a10bc7eb5dc3d4fcd9db63
Villu Ruusmann <villu.ruusmann@gmail.com>
2020-10-11 09:46:52 +0300

    Fixed copyright statements

--- src/main/java/org/jpmml/converter/IndexFeature.java
+++ src/main/java/org/jpmml/converter/IndexFeature.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (c) 2019 Villu Ruusmann
  *
- * This file is part of JPMML-SparkML
+ * This file is part of JPMML-Converter
  *
- * JPMML-SparkML is free software: you can redistribute it and/or modify
+ * JPMML-Converter is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * JPMML-SparkML is distributed in the hope that it will be useful,
+ * JPMML-Converter is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with JPMML-SparkML.  If not, see <http://www.gnu.org/licenses/>.
+ * along with JPMML-Converter.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.jpmml.converter;
 
