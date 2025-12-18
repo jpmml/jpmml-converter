2992d54af4fa207d7fae4e7831f89bd6d1b55c24
Villu Ruusmann <villu.ruusmann@gmail.com>
2025-12-18 22:20:13 +0200

    Imported Java code from the JPMML-Python project

---
+++ pmml-converter/src/main/java/org/jpmml/converter/OperationException.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2025 Villu Ruusmann
+ *
+ * This file is part of JPMML-Converter
+ *
+ * JPMML-Converter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JPMML-Converter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with JPMML-Converter.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jpmml.converter;
+
+public class OperationException extends ConversionException {
+
+	public OperationException(String message){
+		super(message);
+	}
+
+	public OperationException(String message, Throwable cause){
+		super(message, cause);
+	}
+}
