64537f1d71011db074261e8c945721badc62c5a8
Villu Ruusmann <villu.ruusmann@gmail.com>
2026-01-11 12:04:35 +0200

    Imported Java code from the JPMML-SkLearn project

---
+++ pmml-converter/src/main/java/org/jpmml/converter/OrdinalFeature.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2025 Villu Ruusmann
+ *
+ * This file is part of JPMML-Converter
+ *
+ * JPMML-Converter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * JPMML-Converter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with JPMML-Converter.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.jpmml.converter;
+
+import java.util.List;
+
+import org.dmg.pmml.DataType;
+import org.dmg.pmml.Field;
+
+abstract
+public class OrdinalFeature extends DiscreteFeature {
+
+	public OrdinalFeature(PMMLEncoder encoder, Field<?> field, List<?> values){
+		super(encoder, field, values);
+	}
+
+	public OrdinalFeature(PMMLEncoder encoder, Feature feature, List<?> values){
+		super(encoder, feature, values);
+	}
+
+	public OrdinalFeature(PMMLEncoder encoder, String name, DataType dataType, List<?> values){
+		super(encoder, name, dataType, values);
+	}
+
+	abstract
+	public IndexFeature getEncodedFeature();
+
+	@Override
+	public ContinuousFeature toContinuousFeature(){
+		IndexFeature encodedFeature = getEncodedFeature();
+
+		return encodedFeature.toContinuousFeature();
+	}
+
+	@Override
+	public OrdinalFeature expectCardinality(int size){
+		return (OrdinalFeature)super.expectCardinality(size);
+	}
+}
